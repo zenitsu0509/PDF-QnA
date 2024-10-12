343ea8de32edea2a0071db3ae582991fb40e92b8
hackerX400 <gangwarsanu8@gmail.com>
2024-10-12 10:46:15 +0530

    added code

---
+++ streamlit.py
@@ -0,0 +1,83 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from transformers import pipeline, T5Tokenizer, T5ForConditionalGeneration
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+import base64
+
+# Load environment variables
+load_dotenv()
+
+# Model and tokenizer loading
+model_name = "MBZUAI/LaMini-Flan-T5-248M"
+token = os.getenv("api_key")
+
+#model and tokenizer loading
+tokenizer = T5Tokenizer.from_pretrained(model_name, legacy=False)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+# File loader and preprocessing
+def file_preprocessing(file):
+    loader = PyPDFLoader(file)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    texts = text_splitter.split_documents(pages)
+    final_texts = ""
+    for text in texts:
+        final_texts += text.page_content
+    return final_texts
+
+#LLM pipeline
+def llm_pipeline(filepath):
+    pipe_sum = pipeline(
+        'summarization',
+        model = base_model,
+        tokenizer = tokenizer,
+        max_length = 500, 
+        min_length = 50)
+    input_text = file_preprocessing(filepath)
+    result = pipe_sum(input_text)
+    result = result[0]['summary_text']
+    return result
+
+@st.cache_data
+#function to display the PDF of a given file 
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+#streamlit code 
+st.set_page_config(layout="wide")
+
+def main():
+    st.title("Document Summarization App using Langauge Model")
+
+    uploaded_file = st.file_uploader("Upload your PDF file", type=['pdf'])
+
+    if uploaded_file is not None:
+        if st.button("Summarize"):
+            col1, col2 = st.columns(2)
+            filepath = "data/"+uploaded_file.name
+            with open(filepath, "wb") as temp_file:
+                temp_file.write(uploaded_file.read())
+            with col1:
+                st.info("Uploaded File")
+                pdf_view = displayPDF(filepath)
+
+            with col2:
+                summary = llm_pipeline(filepath)
+                st.info("Summarization Complete")
+                st.success(summary)
+
+
+
+if __name__ == "__main__":
+    main()
