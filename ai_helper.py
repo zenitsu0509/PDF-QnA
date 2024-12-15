7b0420f10d2e4f22cdf018536ba30613e3e0dad2
Himanshu Gangwar <gangwarsanu8@gmail.com>
2024-12-15 18:44:32 +0530

    Update ai_helper.py
    remove comment

--- ai_helper.py
+++ ai_helper.py
@@ -16,12 +16,7 @@ load_dotenv()
 class PDFQuestionAnswerer:
     def __init__(self, embedding_model: str = "embed-english-v3.0", 
                  llm_model: str = "command-nightly"):
-        """
-        Initialize the PDF Question Answerer
-        
-        :param embedding_model: Cohere embedding model to use
-        :param llm_model: Cohere language model to use
-        """
+
         # Initialize Cohere Embeddings
         self.embeds = CohereEmbeddings(
             cohere_api_key=st.secrets["COHERE_API_KEY"],
